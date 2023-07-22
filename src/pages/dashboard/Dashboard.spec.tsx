6586811ff24661065b096fafe5b6cc8e9d19dad2
emmanuel okuchukwu <emmanuelokuchukwu@emmanuels-MacBook-Pro.local>
2023-07-22 23:45:58 +0100

    finished testing the dashboard page

--- src/pages/dashboard/Dashboard.spec.tsx
+++ src/pages/dashboard/Dashboard.spec.tsx
@@ -1,17 +1,53 @@
 import { create } from 'react-test-renderer';
+import { render, act, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
 import Dashboard from './Dashboard';
+import * as api from '../../api/journal';
 
 // Mock the dependencies used in the Dashboard component
-jest.mock('../../api/journal', () => ({
-  getJournal: jest.fn().mockResolvedValue([]), // Return an empty array for simplicity
-  onCreateJournal: jest.fn().mockResolvedValue({ id: 'mocked_id', title: 'Mocked Journal', description: 'Mocked Description' }),
-}));
+jest.mock('../../api/journal');
 
-// Snapshot test for the Dashboard component
+const mockJournal = {
+  title: 'Homer Simpson',
+  description: 'Homer Simpson is a family man!',
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  id: '643db8c388f22f9d7395a0f5'
+}
+
 describe('Dashboard component', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  // Snapshot test for the Dashboard component
   it('renders correctly', () => {
+    (api.getJournal as jest.MockedFunction<typeof api.getJournal>).mockResolvedValue([mockJournal])
+
     const tree = create(<Dashboard />).toJSON();
-    console.log(tree);
     expect(tree).toMatchSnapshot();
+  });
+
+  it('should render component', async () => {
+    (api.getJournal as jest.MockedFunction<typeof api.getJournal>).mockResolvedValue([mockJournal])
+
+    await act(async () => {
+      render(<Dashboard />)
+    });
+    expect(screen.getByText('Homer Simpson')).toBeInTheDocument
+  });
+
+  it('should add a new journal entry', async () => {
+    (api.getJournal as jest.MockedFunction<typeof api.getJournal>).mockResolvedValue([]);
+    (api.onCreateJournal as jest.MockedFunction<typeof api.onCreateJournal>).mockResolvedValue(mockJournal);
+
+    render(<Dashboard />)
+
+    await userEvent.type(screen.getByText('Title'), 'Homer Simpson')
+    await userEvent.type(screen.getByText('Description'), 'Homer Simpson is a family man!')
+    await userEvent.click(screen.getByText('Submit'))
+
+    await waitFor(() => expect(screen.getByText('Homer Simpson')).toBeInTheDocument)
   });
 });
